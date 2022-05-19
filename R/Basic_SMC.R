3b2dc3c2c17b8359ab9913ec0e1e5fc7b6c55580
Josh Givens <cn21903@bristol.ac.uk>
2022-05-19 16:03:41 +0100

    Fixed bugs in both SMC approaches

--- R/Basic_SMC.R
+++ R/Basic_SMC.R
@@ -21,22 +21,22 @@ Basic_SMC <- function(init_sample,proposal_sample, proposal_lik, true_lik, g_lik
   
   # DO first sample
   for(i in 1:n){
-    Xmat[i,2]=proposal_sample(Xmat[i,t-1])
-    Wmat[i,2] <- true_lik(x_new[i],Xmat[i,t-1])*g(y[t],x_new[i])/
-      proposal_lik(x_new[i],Xmat[i,t-1])
+    Xmat[i,2]=proposal_sample(Xmat[i,1])
+    Wmat[i,2] <- true_lik(Xmat[i,2],Xmat[i,1])*g_lik(y[t],Xmat[i,2])/
+      proposal_lik(Xmat[i,2],Xmat[i,1])
   }
   Wmat[,2]=Wmat[,2]/sum(Wmat[,2])
   
   
   for (t in 3:timesteps){
     # resample previous iter to get new points
-    x_sampled <- sample(Xmat[,j-1],size=n,replace=TRUE,prob=Wmat[,j-1])
+    x_sampled <- sample(Xmat[,t-1],size=n,replace=TRUE,prob=Wmat[,t-1])
     
     for(i in 1:n){
       #Get new sample
       Xmat[i,t]=proposal_sample(x_sampled[i])
       #Get weights associated with new sample
-      Wmat[i,t] <- true_lik(Xmat[i,t],Xmat[i,t-1])*g(y[t],Xmat[i,t])/
+      Wmat[i,t] <- true_lik(Xmat[i,t],Xmat[i,t-1])*g_lik(y[t],Xmat[i,t])/
         proposal_lik(Xmat[i,t],Xmat[i,t-1])
     }
     #Normalise weights
--- R/diff_SMC.R
+++ R/diff_SMC.R
@@ -18,34 +18,37 @@ diff_SMC <- function(g,p_delta,M_T_samplers,M_T_lik,n=1000,timesteps=100,varphi=
   # Set up matrix for weights
   Wmat <- matrix(NA,n,timesteps)
   gmat <- matrix(NA,n,timesteps)
+  gmat_transf <- matrix(NA,n,timesteps)
   w_new <- rep(NA,n)
   # 1st sample points from initial distribution
   
   #Calculate init g-value  
-  g_0=abs(g(x_0))^varphi
+  g_0=g(x_0)
+  g_0_transf=abs(g_0)^varphi
   for (i in 1:n){
     Xmat[i,1] <- M_T_samplers[[1]](x_0)
-    gmat[i,1] <- abs(g(Xmat[i,1]))^varphi
-    Wmat[i,1] <- gmat[i,1]*p_delta(Xmat[i,1],x_0)/
-      (g_0*M_T_lik[[1]](Xmat[i,1],x_0))
+    gmat[i,1] <- g(Xmat[i,1])
+    gmat_transf[i,1] <- abs(gmat[i,1])^varphi 
+    Wmat[i,1] <- gmat_transf[i,1]*p_delta(Xmat[i,1],x_0)/
+      (g_0_transf*M_T_lik[[1]](Xmat[i,1],x_0))
   }
   #renormalise weights
-  Wmat[,1] <- Wmat[,1]/sum(Wmat[,1])
+  Wmat[,1] <- Wmat[,1]
   
   for (t in 2:timesteps){
     # resample previous iter to get new points
-    x_sampled <- sample(Xmat[,j-1],size=n,replace=TRUE,prob=Wmat[,j-1])
+    x_sampled <- sample(Xmat[,t-1],size=n,replace=TRUE,
+                        prob=Wmat[,t-1]/sum(Wmat[,t-1]))
     
     for(i in 1:n){
       #Get new sample
-      Xmat[i,j]=M_T_samplers[[t]](x_sampled[i])
+      Xmat[i,t]=M_T_samplers[[t]](x_sampled[i])
       #Get weights associated with new sample
-      gmat[i,t] <- abs(g(Xmat[i,t]))^varphi
+      gmat[i,t] <- g(Xmat[i,t])
+      gmat_transf[i,t] <- abs(gmat[i,t])^varphi
       Wmat[i,t] <- gmat[i,t]*p_delta(Xmat[i,t],Xmat[i,t-1])/
         (gmat[i,t-1]*M_T_lik[[1]](Xmat[i,t],Xmat[i,t-1]))
     }
-    #Normalise weights
-    Wmat[,t]=Wmat[,t]/sum(Wmat[,t])
   }
-  return(list(Xmat=Xmat, Wmat=Wmat))
+  return(list(Xmat=Xmat, Wmat=Wmat, gmat=gmat))
 }
