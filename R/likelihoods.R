d8e43525f2a44b59998ecde995aa524768347a6e
Josh Givens <cn21903@bristol.ac.uk>
2022-05-24 16:01:24 +0100

    Added sin sampler

--- R/likelihoods.R
+++ R/likelihoods.R
@@ -1,13 +1,20 @@
 brown_lik <- function(x,x_0,t,sigma,log=F){
   sd <- sqrt(t)*sigma
-  dnorm(x,mean=x_0,sd=sd,log=log)
+  return(dnorm(x,mean=x_0,sd=sd,log=log))
 }
 
 ornuhl_lik <- function(x,x_0,t,alpha,sigma,log=F){
   sd=sqrt(sigma*(1-exp(-2*alpha*t))/alpha)
   mean=x_0*exp(-alpha*t)
-  dnorm(x,mean=mean,sd=sd,log = log)
+  return(dnorm(x,mean=mean,sd=sd,log = log))
 }
+
+sin_lik <- function(x,x_0,t,omega,log=F){
+  mean <- sin(x_0-omega)*t+x_0
+  return(dnorm(x,mean=mean,sd=t,log=log))
+}
+
+
 
 
 
--- R/samplers.R
+++ R/samplers.R
@@ -3,8 +3,13 @@ brown_samp <- function(n,x_0,t,sigma){
   rnorm(n,mean=x_0,sd=sd)
 }
 
-ornuhl_sample <- function(n,x_0,t,sigma,alpha){
+ornuhl_samp <- function(n,x_0,t,sigma,alpha){
   sd=sqrt(sigma*(1-exp(-2*alpha*t))/alpha)
   mean=x_0*exp(-alpha*t)
   rnorm(n,mean=mean,sd=sd)
+}
+
+sin_samp <- function(n,x_0,t,omega){
+  mean <- sin(x_0-omega)+x_0*t
+  rnorm(n,mean=mean,sd=t)
 }
